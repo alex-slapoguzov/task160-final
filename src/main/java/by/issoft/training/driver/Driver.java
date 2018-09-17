335e1af7a99e943873981c0e8dca737f11832d3b
Alexey Slapoguzov <alex.slapoguzov@gmail.com>
2018-09-17 16:19:45 +0300

    add implicitlyWait

--- src/main/java/by/issoft/training/driver/Driver.java
+++ src/main/java/by/issoft/training/driver/Driver.java
@@ -7,24 +7,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    private static WebDriver driver;
-    private final static String HOME_URL = "https://gmail.com";
+	private static WebDriver driver;
+	private final static String HOME_URL = "https://gmail.com";
 
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        }
-        return driver;
-    }
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		}
+		return driver;
+	}
 
-    public static void openHomePage() {
-        getDriver().get(HOME_URL);
-    }
+	public static void openHomePage() {
+		getDriver().get(HOME_URL);
+	}
 
-    public static void closeDriver() {
-        getDriver().quit();
-        driver = null;
-    }
+	public static void closeDriver() {
+		getDriver().quit();
+		driver = null;
+	}
 }
