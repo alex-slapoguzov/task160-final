c1d336ac157aa84a1ec76e1cfbc40ad27c86d61a
Alexey Slapoguzov <alex.slapoguzov@gmail.com>
2018-09-18 15:24:43 +0300

    change driver

--- src/main/java/by/issoft/training/driver/Driver.java
+++ src/main/java/by/issoft/training/driver/Driver.java
@@ -2,20 +2,33 @@ package by.issoft.training.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
 	private static WebDriver driver;
+	private static String browser;
 	private final static String HOME_URL = "https://gmail.com";
 
 	public static WebDriver getDriver() {
 		if (driver == null) {
-			driver = new ChromeDriver();
+			if (browser != null) {
+				switch (browser) {
+					case "Firefox":
+						driver = new FirefoxDriver();
+						break;
+					case "Chrome":
+						driver = new ChromeDriver();
+						break;
+				}
+			} else {
+				driver = new ChromeDriver();
+			}
 			driver.manage().window().maximize();
-			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
 		}
 		return driver;
 	}
